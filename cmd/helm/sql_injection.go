8e7b99af024ad4166d4ebf931b88e97db2117990
Van(sunan) <vandx@vmodev.com>
2022-05-31 23:32:46 +0700

    add run test coverage

--- cmd/helm/sql_injection.go
+++
@@ -1,7 +0,0 @@
-func buildSql(email string) string {
-	return fmt.Sprintf("SELECT * FROM users WHERE email='%s';", email)
-  }
-  
-  buildSql("oyetoketoby80@gmail.com")
-  
-  // SELECT * FROM users WHERE email='oyetoketoby80@gmail.com';
